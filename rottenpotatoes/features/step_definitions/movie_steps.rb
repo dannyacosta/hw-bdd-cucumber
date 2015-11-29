a2c257b9c27064da467871169c975022770f1b7d
djacosta <danny.acosta@gmail.com>
2015-11-29 06:37:39 +0000

    Completed all parts

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,34 +1,57 @@
-# Add a declarative step here for populating the DB with movies.
-
+#Background
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create({:title => movie[:title], :rating => movie[:rating], :release_date => movie[:release_date]})
   end
-  fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+Then /I should see (.*) before (.*)/ do |e1, e2|
+  within_table "movies" do
+    match = /"#{e1}.*#{e2}"/m =~ page.body
+  end
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
-
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+When /I check the following ratings: (.*)/ do |rating_list|
+  rating_list.split(',').each do |rating|
+      check("ratings[#{rating.gsub(' ','')}]")
+  end
 end
+
+When /I uncheck the following ratings: (.*)/ do |rating_list|
+  rating_list.split(',').each do |rating|
+      uncheck("ratings[#{rating.gsub(' ','')}]")
+  end
+end
+
+When /I click on (.*)$/ do |button|
+  click_button("ratings_#{button}")
+end
+
+Then /I should see movies with the following ratings: (.*)$/ do |rating_list|
+  rating_list.split(',').each do |rating|
+    rating.gsub!(' ', '')
+    within_table "movies" do
+      expect(page).to have_content(rating)
+    end
+  end
+end
+
+Then /I should not see movies with the following ratings: (.*)$/ do |rating_list|
+  rating_list.split(',').each do |rating|
+    rating.gsub!(' ', '')
+    within_table "movies" do
+      expect(page).to have_no_content(rating)
+    end
+  end
+end
+
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  count = Movie.all.count
+  page.all('table#movies tr').count.should == count + 1
 end
